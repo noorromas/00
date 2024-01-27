d0786186dab513d71ac2f633ca49e9b13e5bbbfe
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-01-28 02:52:08 +0300

    Add files via upload

---
+++ nour5.js
@@ -0,0 +1,16 @@
+﻿`);
+var l_itvaiah=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvaiah);
+        $("#users .uzr:contains('نـَوَر')").addClass('nour1');
+        $("div#users .uzr.nour1 .My_vedio1").click(function(){
+        $("#movon1").show();
+        $('#movon1').addClass('nour1');
+        
+        $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 10000);
+
